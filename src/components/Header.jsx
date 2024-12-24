f3c262d084071fb9c70bf41ba6e5f9e295dd5a5f
Nagender kumar <kumarnagender811@gmail.com>
2024-12-24 16:50:02 +0530

    refactor Header component for improved layout and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,10 +35,7 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a
-          className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4"
-          href="#hero"
-        >
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
